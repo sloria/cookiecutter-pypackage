815dbbf82d3319e7d09e7df71f4cdf040197111a
Steven Loria <sloria1@gmail.com>
2013-09-28 12:27:15 -0500

    Prevent circular import in setup.py
    - Add find_version method

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -1,7 +1,8 @@
+# -*- coding: utf-8 -*-
+import re
 import sys
 import os
 import subprocess
-import {{cookiecutter.repo_name}}
 
 try:
     from setuptools import setup, find_packages
@@ -34,6 +35,24 @@ except ImportError:
         return out
 
     find_packages = _find_packages
+
+
+def find_version(fname):
+    '''Attempts to find the version number in the file names fname.
+    Raises RuntimeError if not found.
+    '''
+    version = ''
+    with open(fname, 'r') as fp:
+        reg = re.compile(r'__version__ = [\'"]([^\'"]*)[\'"]')
+        for line in fp:
+            m = reg.match(line)
+            if m:
+                version = m.group(1)
+                break
+    if not version:
+        raise RuntimeError('Cannot find version information')
+
+__version__ = find_version("{{ cookiecutter.repo_name }}/__init__.py")
 
 PUBLISH_CMD = "python setup.py register sdist bdist_wheel upload"
 TEST_PUBLISH_CMD = 'python setup.py register -r test sdist bdist_wheel upload -r test'
@@ -74,7 +93,7 @@ def read(fname):
 
 setup(
     name='{{ cookiecutter.repo_name }}',
-    version={{ cookiecutter.repo_name }}.__version__,
+    version=__version__,
     description='{{ cookiecutter.short_description }}',
     long_description=(read("README.rst") + '\n\n' +
                         read("HISTORY.rst")),
