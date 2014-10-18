f845d5a871d38d13afb19630cefe99d7bb86d746
sloria <sloria1@gmail.com>
2014-10-18 09:21:52 -0400

    Add some useful Sphinx plugins

--- {{cookiecutter.repo_name}}/docs/conf.py
+++ {{cookiecutter.repo_name}}/docs/conf.py
@@ -26,10 +26,19 @@ sys.path.append(os.path.abspath("_themes"))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
+]
 
 primary_domain = 'py'
 default_role = 'py:obj'
+
+intersphinx_mapping = {
+    'python': ('http://python.readthedocs.org/en/latest/', None),
+}
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
